d956319be25ccd34b0d204aa7a8af8d04502434a
Jerónimo Córdoba <jerof.l.cordoba01@gmail.com>
2024-03-01 08:31:18 -0300

    aaa

--- Parcial/Parcial.py
+++ Parcial/Parcial.py
@@ -11,7 +11,23 @@ apellido: Córdoba
 Ejercicio: Parcial 
 ---
 Parcial Turno Mañana:
-
+De 5 mascotas que ingresan a una veterinaria se deben tomar y validar los siguientes datos.
+Nombre
+Tipo (gato ,perro o exotico)
+Peso ( entre 10 y 80)
+Sexo( F o M )
+Edad(mayor a 0)
+Pedir datos por prompt y mostrar por print, se debe informar:
+Informe A- Cuál fue el sexo menos ingresado (F o M)
+Informe B- El porcentaje de mascotas hay por tipo (gato ,perro o exotico)
+Informe C- El nombre y tipo de la mascota menos pesada
+Informe D- El nombre del perro más joven
+Informe E- El promedio de peso de todas las mascotas
+Condiciones de aprobación:
+Se debe realizar el ingreso de datos de manera correcta con las validaciones correspondientes siguiendo las
+reglas de estilo de la cátedra y al menos uno de los informes solicitados de manera perfecta para obtener una
+nota 6(seis)
+Cada informe adicional logrado correctamente suma un punto más hasta obtener nota 10(diez)
 '''
 
 class App(customtkinter.CTk):
